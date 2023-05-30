7235664caf3dd4d0ae868fccb65be5d01bc175a6
adityaShar24 <adityarajansharma1234@gmail.com>
2023-05-30 14:24:01 +0530

    added files

--- src/routes/blog_router.py
+++
@@ -1,9 +0,0 @@
-from controllers.blog_controller import Blog
-from flask import Blueprint
-
-blog_bp = Blueprint('blog' , __name__)
-
-@blog_bp.post('/blog')
-def Blog_wrapper():
-    return Blog()
-
--- src/routes/commet_router.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from controllers.comments_controller import Comment
-
-com_bp = Blueprint('com_bp' , __name__)
-
-@com_bp.post("/comment")
-def comment_wrapper():
-    return Comment()
