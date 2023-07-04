0c7186fb6c39397c393e5b7fb73ca1d6e4fa4672
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-04 11:32:21 +0000

    Added reply_comment route

--- src/routes/commet_router.py
+++ src/routes/commet_router.py
@@ -1,8 +1,14 @@
 from flask import Blueprint
-from controllers.comments_controller import Comment
+from controllers.comments_controller import comment , reply
+from flask_jwt import jwt_required
 
 com_bp = Blueprint('com_bp' , __name__)
 
 @com_bp.post("/comment")
+@jwt_required()
 def comment_wrapper():
-    return Comment()
+    return comment()
+
+@com_bp.post('/reply_comment')
+def reply_comment_wrapper():
+    return reply()
