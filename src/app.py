c4ee6834c92d1550befcb4e506439b9cf540ddcf
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-04 11:33:49 +0000

    refactor app

--- src/app.py
+++ src/app.py
@@ -2,7 +2,9 @@ from flask import Flask
 from routes.auth_router import auth_bp
 from routes.blog_router import blog_bp
 from routes.commet_router import com_bp
+from flask_jwt_extended import JWTManager
 
+jwt = JWTManager(app)
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "my_secret_key"
 
