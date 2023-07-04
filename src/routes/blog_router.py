b6ab1037f36529e12bf7d60f0f01c07950763093
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-04 11:31:33 +0000

    Added allBlogs route

--- src/routes/blog_router.py
+++ src/routes/blog_router.py
@@ -1,9 +1,15 @@
-from controllers.blog_controller import Blog
+from controllers.blog_controller import Blog , GetAllBLogs
+from flask_jwt import jwt_required
 from flask import Blueprint
 
 blog_bp = Blueprint('blog' , __name__)
 
 @blog_bp.post('/blog')
+@jwt_required()
 def Blog_wrapper():
     return Blog()
 
+@blog_bp.get('/allBlogs')
+@jwt_required()
+def GetAllBLogs_wrapper():
+    return GetAllBLogs
