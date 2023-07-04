65ea283ad752843a3485c19c9a8880dfe8bc1b0d
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-07-04 11:25:56 +0000

    Added reply function in comments_controller

--- src/controllers/comments_controller.py
+++ src/controllers/comments_controller.py
@@ -1,16 +1,15 @@
 from flask import request , make_response
-from models.comments_models import comments
+from models.comments_models import Comment
 import json 
 import bson.json_util as json_util
 
 
-def Comment():
+def comment():
     body = json.loads(request.data)
     comment = body['comment']
     blog_ID = body['blog_ID']
-    user_ID = body['user_ID']
-    
-    all_comments = comments(comment = comment , blog_ID=blog_ID , user_ID= user_ID)
+    user_ID = body['user_ID']    
+    all_comments = Comment(comment = comment , blog_ID=blog_ID , user_ID= user_ID)
     if not all_comments.blog_ID and all_comments.comment:
         return make_response({"message":"comment and blog_ID cannnot be empty"} , 401)
     
@@ -18,4 +17,17 @@ def Comment():
     json_Version = json_util.dumps(saved_comments)
     return make_response({"message":'Your comment has been saved succesfully' , 'Comment':json_Version} , 201)
 
-
+def reply():
+    body = json.loads(request.data)
+    comment = body['comment']
+    blog_ID = body['blog_ID']
+    user_ID = body['user_ID']    
+    parent_comment_ID = body['parent_comment_ID']
+    reply = body['reply']
+    Comments = Comment(comment = comment , user_ID = user_ID , blog_ID = blog_ID , parent_comment_ID = parent_comment_ID)
+    if not comment and blog_ID and user_ID:
+        return make_response({"message": "comment, blog_ID and user_ID cannot be empty"} , 401)
+    
+    reply_comment = Comments.reply_comment(reply)
+    print(reply_comment)
+    return make_response({'message':'Your reply has been saved successfully' , 'reply': reply_comment} , 201)
